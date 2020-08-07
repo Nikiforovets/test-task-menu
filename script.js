360a683fc59ea5ed048545372c4d9902899278eb
Maksim <nikimax@tut.by>
2020-08-07 14:55:00 +0300

    Adaptive for 1024px

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 for (let i = 1; i <= 6; i++) {
     document.getElementById(`nav-elem${i}`).addEventListener("mouseover", () => {
-        // document.getElementById(`submenu${i}`).style.height = 'auto';
-        // document.getElementById(`submenu${i}`).style.overflow = 'visible';
-        // document.getElementById(`submenu${i}`).style.opacity = 1;
-        document.getElementById(`submenu${i}`).style.display = 'flex';
-        document.getElementById('dark-background').style.display = 'block';
+        document.getElementById(`submenu${i}`).style.opacity = 1;
+        document.getElementById(`submenu${i}`).style.visibility = 'visible';
+        document.getElementById('dark-background').style.opacity = 0.3;
+        document.getElementById('dark-background').style.visibility = 'visible';
     });
 
     document.getElementById(`nav-elem${i}`).addEventListener("mouseout", () => {
-        // document.getElementById(`submenu${i}`).style.height = '0px';
-        // document.getElementById(`submenu${i}`).style.overflow = 'hidden';
-        // document.getElementById(`submenu${i}`).style.opacity = 0;
-        document.getElementById(`submenu${i}`).style.display = 'none';
-        document.getElementById('dark-background').style.display = 'none';
+        document.getElementById(`submenu${i}`).style.opacity = 0;
+        document.getElementById(`submenu${i}`).style.visibility = 'hidden';
+        document.getElementById('dark-background').style.opacity = 0;
+        document.getElementById('dark-background').style.visibility = 'hidden';
     });
 }
 
