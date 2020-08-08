a4cc38c3eca76dbeee0f1749974ae78ae54f9691
Maksim <nikimax@tut.by>
2020-08-08 20:20:03 +0300

    Laptop top level menu

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ for (let i = 1; i <= 6; i++) {
     } else {
         addMobileListeners(submenu, i);
     }
+}
 
+if (screen.width < 768) {
+    createArrows();
 }
 
 function addDesktopListeners(submenu, background, i) {
@@ -25,9 +28,33 @@ function addDesktopListeners(submenu, background, i) {
 }
 
 function addMobileListeners(submenu, i) {
-    document.getElementById(`nav-elem${i}`).addEventListener("click", () => {
+    document.getElementById(`nav-elem${i}`).addEventListener("click", function () {
+        const activeTabs = document.getElementsByClassName('elem active');
+        for (tab of activeTabs) {
+            if (document.getElementById(`elem${i}`) != tab)
+                tab.className = 'elem';
+        }
+        const activeSubmenues = document.getElementsByClassName('submenu active');
+        for (activeSubmenu of activeSubmenues) {
+            if (activeSubmenu != submenu)
+                activeSubmenu.className = 'submenu';
+        }
         submenu.classList.toggle('active');
-    })
+        for (element of this.childNodes) {
+            if (element.className == 'elem' || element.className == 'elem active') {
+                element.classList.toggle('active');
+            }
+        }
+    });
+}
+
+function createArrows() {
+    const elements = document.getElementsByClassName('elem');
+    for (const element of elements) {
+        let arrow = document.createElement('div');
+        arrow.classList.add('arrow');
+        element.appendChild(arrow);
+    }
 }
 
 
